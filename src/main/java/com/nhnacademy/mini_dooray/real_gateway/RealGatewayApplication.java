4f20de3b76a01a412bdd764e03975b3e8e74a119
김희망 <gmlakd990514@naver.com>
2024-10-14 17:46:59 +0900

    init

---
+++ src/main/java/com/nhnacademy/mini_dooray/real_gateway/RealGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.mini_dooray.real_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhnacademy/mini_dooray/real_gateway/RealGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.mini_dooray.real_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
